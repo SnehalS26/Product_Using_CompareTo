755a35a2df1fd97326c0399a477d4cd0bb6c4af6
SnehalS26 <Snehalshirsath26@gmail.com>
2023-07-11 17:38:30 +0530

    Add project files.

---
+++ Product_Using_CompareTo/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Using_CompareTo
+{
+    public class Product:IComparable<Product>
+    {
+        private String name;
+        private int price;
+        public Product(String name , int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        public int CompareTo(Product other)
+        {
+            if(this.price < other.price)
+            {
+                return -1;
+            }
+            else if(this.price > other.price)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{name} {price}";
+        }
+    }
+}
---
+++ Product_Using_CompareTo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Using_CompareTo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Laptop = new Product("Laptop", 2000);
+            Product Mobile = new Product("Mobile", 1500);
+
+            int result = Laptop.CompareTo(Mobile);
+
+            if (result < 0)
+            {
+                Console.WriteLine("Laptop has less price than Mobile");
+            }
+            else if (result > 0)
+            {
+                Console.WriteLine("Laptop has more price than Mobile");
+            }
+            else
+            {
+                Console.WriteLine("Laptop & Mobile has same price ");
+            }
+
+        }
+    }
+}
